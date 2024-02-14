94347e6f56ca33dfdd245e0068258e5b56d01773
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2024-02-14 16:32:52 +0100

    first push of 'Simple Counter'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCounter] = useState(0);
+
+  function onCLickAdd() {
+    setCounter(count + 1);
+  }
+
+  function onCLickDim() {
+    setCounter(count - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onMouseUp={onCLickAdd}
+        style={{
+          padding: "10px",
+          fontSize: "20px",
+          backgroundColor: "lightblue",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Aggiungi
+      </button>
+      <p style={{ fontSize: "20px", color: count < 0 ? "red" : "black" }}>
+        {count}
+      </p>
+      <button
+        onMouseUp={onCLickDim}
+        style={{
+          padding: "10px",
+          fontSize: "20px",
+          backgroundColor: "lightblue",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Diminuisci
+      </button>
     </div>
   );
 }
